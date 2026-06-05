c57c23adbfbaeb6e586de96b24a3f3c24f61a1cc
miftahstudy <159855764+miftahstudy@users.noreply.github.com>
2026-06-05 09:42:05 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
@@ -6,11 +5,6 @@ import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
-
-# Set MLflow Tracking URI lokal
-mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment("Loan Approval Prediction")
 
 
 def load_data():
@@ -34,29 +28,32 @@ def train_model():
         stratify=y
     )
 
-    # Basic requirement: menggunakan autolog MLflow
     mlflow.sklearn.autolog()
 
-    with mlflow.start_run(run_name="RandomForest_Autolog"):
-        model = RandomForestClassifier(
-            n_estimators=100,
-            random_state=42
-        )
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
 
-        model.fit(X_train, y_train)
+    model.fit(X_train, y_train)
 
-        y_pred = model.predict(X_test)
+    y_pred = model.predict(X_test)
 
-        accuracy = accuracy_score(y_test, y_pred)
-        precision = precision_score(y_test, y_pred)
-        recall = recall_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
 
-        print("Training selesai.")
-        print(f"Accuracy : {accuracy:.4f}")
-        print(f"Precision: {precision:.4f}")
-        print(f"Recall   : {recall:.4f}")
-        print(f"F1-score : {f1:.4f}")
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1_score", f1)
+
+    print("Training selesai.")
+    print(f"Accuracy : {accuracy:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall   : {recall:.4f}")
+    print(f"F1-score : {f1:.4f}")
 
 
 if __name__ == "__main__":
